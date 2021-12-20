52a5320ec2e674f12d16409a4c4c187a54ce92ce
Nhut <trannhut1707@gmail.com>
2021-12-20 16:49:07 +0700

    Rearrange folder for deploy

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports ={
     entry: './src/app.js',
     output: {
-        path: path.join(__dirname, '../server/public'),
+        path: path.join(__dirname, '../public'),
         filename: 'bundle.js'
     },
      devtool:'source-map',
